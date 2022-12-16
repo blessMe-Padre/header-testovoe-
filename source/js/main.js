76a77b2bc3e57caede4edc7a16cbc1535c739183
Сергей Субботин <zarodiny@yandex.ru>
2022-12-16 14:40:37 +1000

    добавляет выпадающий блок

--- source/js/main.js
+++ source/js/main.js
@@ -1,73 +1,72 @@
 /* eslint-disable */
 
 window.addEventListener('DOMContentLoaded', () => {
-    // Modules
+  // Modules
 
-    // ---------------------------------
-    const main = document.querySelector('.main');
-    const searchInput = document.querySelector('.input');
+  // ---------------------------------
+  const searchInput = document.querySelector('.input');
+  const regionList = document.querySelector('.region__list');
 
-    //запрос на сервер - в ответе массив регионов
-    const postRequest = () => {
-        fetch('https://studika.ru/api/areas', {
-            method: 'POST',
-        })
-            .then(data => data.json())
-            .then(data => {
-                getSearch(data)
-            })
-            .catch(err => {
-                console.log('Ошибка', err);
-            })
-    }
-    postRequest();
+  //запрос на сервер - в ответе массив регионов
+  const postRequest = () => {
+    fetch('https://studika.ru/api/areas', {
+      method: 'POST',
+    })
+      .then(data => data.json())
+      .then(data => {
+        getSearch(data)
+      })
+      .catch(err => {
+        console.log('Ошибка', err);
+      })
+  }
+  postRequest();
 
-    //распечатывает названия регионов в main
-    const printToHtml = (regions) => {
-        main.insertAdjacentHTML('afterbegin', `
-            <ul class="list">${setRegionName(regions).join('')}</ul>
-        `
-        );
-    }
+  //распечатывает названия регионов в main
+  const printToHtml = (regions) => {
+    regionList.insertAdjacentHTML('afterbegin',
+      setRegionName(regions).join('')
+    );
+  }
 
-    //перебирает массив с регионами и возвращает название каждого
-    const setRegionName = (regions) => {
-        return regions.map(region => `
+  //перебирает массив с регионами и возвращает название каждого
+  const setRegionName = (regions) => {
+    return regions.map(region => `
             <li>${region.name}</li>
         `);
-    }
+  }
 
-    const getSearch = (data) => {
-        printToHtml(data);
+  const getSearch = (data) => {
+    printToHtml(data);
 
-        searchInput.addEventListener('input', (evt) => {
-            let value = evt.target.value.trim();
-            const liItems = document.querySelectorAll('.list li');
+    searchInput.addEventListener('input', (evt) => {
+      let value = evt.target.value.trim();
+      const liItems = document.querySelectorAll('.list li');
 
-            if (value !== 0) {
-                liItems.forEach(item => {
-                    if (item.innerText.toLowerCase().search(value) == -1) {
-                        item.classList.add('hide');
-                        item.innerHTML = item.innerText;
-                    }
-                    else {
-                        item.classList.remove('hide');
-                        let str = item.innerText;
-                        item.innerHTML = insertMark(str, item.innerText.toLowerCase().search(value), value.length);
-                    }
-                })
-            }
-            else {
-                liItems.forEach(item => {
-                    item.classList.remove('hide');
-                });
-            }
+      if (value !== 0) {
+        liItems.forEach(item => {
+          if (item.innerText.toLowerCase().search(value) == -1) {
+            item.classList.add('hide');
+            item.innerHTML = item.innerText;
+          }
+          else {
+            item.classList.remove('hide');
+            let str = item.innerText;
+            item.innerHTML = insertMark(str, item.innerText.toLowerCase().search(value), value.length);
+          }
+        })
+      }
+      else {
+        liItems.forEach(item => {
+          item.classList.remove('hide');
         });
-    }
+      }
+    });
+  }
 
-    function insertMark(string, pos, len) {
-        return string.slice(0, pos) + '<strong>' + string.slice(pos, pos + len) + '</strong>' + string.slice(pos + len);
-    }
+  function insertMark(string, pos, len) {
+    return string.slice(0, pos) + '<strong>' + string.slice(pos, pos + len) + '</strong>' + string.slice(pos + len);
+  }
 
-    window.addEventListener('load', () => { });
+  window.addEventListener('load', () => { });
 });
